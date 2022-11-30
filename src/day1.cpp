e12759389d80136d85bc142464e5bba127fdc282
Iñigo Moreno <inigo.moreno@tecnalia.com>
2022-11-30 16:17:58 +0100

    Format

--- src/day1.cpp
+++ src/day1.cpp
@@ -3,37 +3,32 @@
 using namespace std;
 using namespace Eigen;
 
-int countAscending(ArrayXi vec)
-{
-    auto ascending = vec.tail(vec.size() - 1) > vec.head(vec.size() - 1);
-    return ascending.count();
+int countAscending(ArrayXi vec) {
+  auto ascending = vec.tail(vec.size() - 1) > vec.head(vec.size() - 1);
+  return ascending.count();
 }
 
-class Today : public Day
-{
+class Today : public Day {
 public:
-    Today() : Day(1) {}
+  Today() : Day(1) {
+  }
 
 protected:
-    ArrayXi vec;
-    int N;
+  ArrayXi vec;
+  int N;
 
-    virtual void parse(istream &in) override
-    {
-        vec = eigenRead<ArrayXi>(in, ' ');
-    }
-    virtual void part1(ostream &out) override
-    {
-        out << countAscending(vec);
-    }
-    virtual void part2(ostream &out) override
-    {
-        out << countAscending(vec.segment(0, vec.size() - 2) + vec.segment(1, vec.size() - 2) + vec.segment(2, vec.size() - 2));
-    }
+  virtual void parse(istream& in) override {
+    vec = eigenRead<ArrayXi>(in, ' ');
+  }
+  virtual void part1(ostream& out) override {
+    out << countAscending(vec);
+  }
+  virtual void part2(ostream& out) override {
+    out << countAscending(vec.segment(0, vec.size() - 2) + vec.segment(1, vec.size() - 2) + vec.segment(2, vec.size() - 2));
+  }
 };
 
-int main()
-{
-    Today day;
-    day.run();
+int main() {
+  Today day;
+  day.run();
 }
