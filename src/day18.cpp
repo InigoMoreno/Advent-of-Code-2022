0e1c283643e69cc363f4a196f16cbf4d8f692b91
Iñigo Moreno <inigo.moreno@tecnalia.com>
2022-12-19 12:57:11 +0100

    Day18

--- src/day18.cpp
+++ src/day18.cpp
@@ -40,11 +40,38 @@ class Today : public Day {
     out << count;
   }
 
-  virtual void part2(ostream& out) override {}
+  virtual void part2(ostream& out) override {
+    int M = droplet.size();
+    int N = droplet[0].size();
+    int O = droplet[0][0].size();
+    VVVB exterior(M, VVB(N, VB(O, false)));
+    queue<tuple<int, int, int>> q;
+    q.push({0, 0, 0});
+    exterior[0][0][0] = true;
+    while (!q.empty()) {
+      auto [x, y, z] = q.front();
+      q.pop();
+      for (auto [dx, dy, dz] : dirs) {
+        if (x + dx < 0 or x + dx >= M) continue;
+        if (y + dy < 0 or y + dy >= N) continue;
+        if (z + dz < 0 or z + dz >= O) continue;
+        if (exterior.at(x + dx).at(y + dy).at(z + dz)) continue;
+        if (droplet.at(x + dx).at(y + dy).at(z + dz)) continue;
+        exterior.at(x + dx).at(y + dy).at(z + dz) = true;
+        q.push({x + dx, y + dy, z + dz});
+      }
+    }
+
+    int count = 0;
+    for (auto [x, y, z] : data)
+      for (auto [dx, dy, dz] : dirs)
+        if (exterior.at(x + dx).at(y + dy).at(z + dz)) count++;
+    out << count;
+  }
 };
 
 int main() {
   Today day;
-  day.input_path = "../input/input{}-example.txt";
+  // day.input_path = "../input/input{}-example.txt";
   day.run();
 }
