db200b0c61ae2453d2d0e76e3809ec46b8af3d67
Iñigo Moreno <inigo.moreno@tecnalia.com>
2022-12-19 09:56:11 +0100

    Finish day17

--- src/day17.cpp
+++ src/day17.cpp
@@ -58,7 +58,10 @@ class Today : public Day {
  protected:
   string pattern;
 
-  virtual void parse(istream& in) override { pattern = streamToString(in); }
+  virtual void parse(istream& in) override {
+    pattern = streamToString(in);
+    pattern.erase(remove_if(pattern.begin(), pattern.end(), ::isspace), pattern.end());
+  }
 
   bool check_pos_piece(lpos p, int piece, const field_type& field) {
     for (int i = 0; i < 4; i++) {
@@ -89,7 +92,13 @@ class Today : public Day {
     lpos p = {3, 4};
     long long int piece = 0;
     long long int max_height = 0;
+    long long int p_max_height = 0;
+    long long int p_piece = 0;
     while (piece < N) {
+      // Use this cout to find the ammount of pieces in a cycle, you should find a cycle and add the sum to the left
+      // cout << piece - p_piece << ' ' << max_height - p_max_height << endl;
+      p_max_height = max_height;
+      p_piece = piece;
       for (char c : pattern) {
         if (c != '>' and c != '<') continue;
         int dir = c == '>' ? 1 : -1;
@@ -116,13 +125,23 @@ class Today : public Day {
     }
   }
 
-  virtual void part1(ostream& out) override { out << generic_part(2022); }
+  virtual void part1(ostream& out) override { out << generic_part(20220); }
 
-  virtual void part2(ostream& out) override { out << generic_part(2022);}
+  virtual void part2(ostream& out) override {
+    long long int N = 1000000000000;
+    long long int start = 5000ll;
+    long long int rep = 1725;  // There's a repetition every 1725 pieces, use cout above to find out this number
+                               // In the example it is 35
+    long long int times = (N - start) / rep;
+    long long int remainder = (N - start) % rep;
+    long long int increase = generic_part(start + rep + remainder) - generic_part(start + remainder);
+    out << generic_part(start + remainder) + times * increase;
+    // out << generic_part(N) << endl;
+  }
 };
 
 int main() {
   Today day;
-  day.input_path = "../input/input{}-example.txt";
+  // day.input_path = "../input/input{}-example.txt";
   day.run();
 }
