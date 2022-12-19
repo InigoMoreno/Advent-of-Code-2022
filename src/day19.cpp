536ed68b40a37b91a25b425596ab20dbeab72ad8
Iñigo Moreno <inigo.moreno@tecnalia.com>
2022-12-19 16:10:00 +0100

    Inefficient day19

--- src/day19.cpp
+++ src/day19.cpp
@@ -35,12 +35,28 @@ struct Cost {
     return os << "Cost(" << d.ore << ',' << d.clay << ',' << d.obsidian << ',' << d.geode << ')';
   }
 };
-struct Blueprint {
+class Blueprint {
+ private:
+  Cost _max;
+  bool max_found = false;
+
+ public:
   Cost ore;
   Cost clay;
   Cost obsidian;
   Cost geode;
-  Blueprint operator+(const Blueprint &other) const { return Blueprint({ore + other.ore, clay + other.clay, obsidian + other.obsidian, geode + other.geode}); }
+  Cost max() {
+    if (!max_found) {
+      for (Cost cost : {ore, clay, obsidian, geode}) {
+        if (cost.ore > _max.ore) _max.ore = cost.ore;
+        if (cost.clay > _max.clay) _max.clay = cost.clay;
+        if (cost.obsidian > _max.obsidian) _max.obsidian = cost.obsidian;
+        if (cost.geode > _max.geode) _max.geode = cost.geode;
+      }
+      max_found = true;
+    }
+    return _max;
+  }
   Cost &operator[](int index) {
     switch (index) {
       case 0:
@@ -96,16 +112,22 @@ class Today : public Day {
       blueprints.push_back(b);
     }
   }
-
+  int best_so_far = 1;
   int dfs(int minutes_left, Cost robots, Cost resources, Blueprint &blueprint) {
-    if (minutes_left == 0) return resources.geode;
+    if (minutes_left == 0) {
+      if (resources.geode > best_so_far) best_so_far = resources.geode;
+      return resources.geode;
+    }
+    if (resources.geode + robots.geode * minutes_left + minutes_left * (minutes_left - 1) / 2 < best_so_far) return 0;
     // int max_geodes = 0;
     int max_geodes = dfs(minutes_left - 1, robots, resources + robots, blueprint);
     for (int i = 3; i >= 0; i--) {
+      if (i != 3 and robots[i] >= blueprint.max()[i]) continue;
       if (resources >= blueprint[i]) {
         Cost new_robots = robots;
         new_robots[i]++;
         int sub_res = dfs(minutes_left - 1, new_robots, (resources - blueprint[i]) + robots, blueprint);
+        if (i == 3) return sub_res;
         if (sub_res > max_geodes) max_geodes = sub_res;
       }
     }
@@ -113,14 +135,28 @@ class Today : public Day {
     // if (max_geodes == 0) max_geodes = dfs(minutes_left - 1, robots, resources + robots, blueprint);
     return max_geodes;
   }
+  virtual void part1(ostream &out) override {
+    int res = 0;
+    for (int i = 0; i < blueprints.size(); i++) {
+      best_so_far = 1;
+      res += dfs(24, {1, 0, 0, 0}, {0, 0, 0, 0}, blueprints[i]) * (i + 1);
+    }
+    out << res;
+  }
 
-  virtual void part1(ostream &out) override { out << dfs(24, {1, 0, 0, 0}, {0, 0, 0, 0}, blueprints[0]); }
-
-  virtual void part2(ostream &out) override {}
+  virtual void part2(ostream &out) override {
+    int res = 1;
+    for (uint i = 0; i < blueprints.size(); i++) {
+      if (i == 3) break;
+      best_so_far = 1;
+      res *= dfs(32, {1, 0, 0, 0}, {0, 0, 0, 0}, blueprints[i]);
+    }
+    out << res;
+  }
 };
 
 int main() {
   Today day;
-  day.input_path = "../input/input{}-example.txt";
+  // day.input_path = "../input/input{}-example.txt";
   day.run();
 }
