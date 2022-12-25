86e51d2e16d0e0d37769cf1994d10768e48c5c5d
Iñigo Moreno <inigo.moreno@tecnalia.com>
2022-12-25 15:12:31 +0100

    Make day25 more object-oriented

--- src/day25.cpp
+++ src/day25.cpp
@@ -4,6 +4,38 @@ using namespace std;
 // using namespace Eigen;
 // using namespace absl;
 
+map<char, int> table = {
+    {'=', -2}, {'-', -1}, {'0', 0}, {'1', 1}, {'2', 2},
+};
+
+typedef long long int INT;
+
+INT snafuToDecimal(string snafu) {
+  INT decimal = 0;
+  for (int i = 0; i < snafu.size(); i++) decimal += table[snafu[snafu.size() - 1 - i]] * pow(5, i);
+  return decimal;
+}
+
+string lookup = "=-012";
+string decimalToSnafu(INT decimal) {
+  auto [q, r] = std::div(decimal + 2, (INT)5);
+  return (q ? decimalToSnafu(q) : "") + lookup[r];
+}
+
+class Snafu {
+ public:
+  mutable INT _decimal;
+
+ public:
+  Snafu(INT decimal) : _decimal(decimal){};
+  Snafu(string snafu) : _decimal(snafuToDecimal(snafu)){};
+  const Snafu& operator+=(const Snafu& other) const {
+    this->_decimal =  this->_decimal + other._decimal;
+    return *this;
+   }
+  friend std::ostream& operator<<(std::ostream& os, const Snafu& d) { return os << decimalToSnafu(d._decimal); }
+};
+
 class Today : public Day {
  public:
   Today() : Day(25) {}
@@ -12,28 +44,10 @@ class Today : public Day {
   vector<string> snafus;
   virtual void parse(istream& in) override { snafus = absl::StrSplit(streamToString(in), "\n", absl::SkipEmpty()); }
 
-  map<char, int> table = {
-      {'=', -2}, {'-', -1}, {'0', 0}, {'1', 1}, {'2', 2},
-  };
-
-  typedef long long int INT;
-
-  INT snafuToDecimal(string snafu) {
-    INT decimal = 0;
-    for (int i = 0; i < snafu.size(); i++) decimal += table[snafu[snafu.size() - 1 - i]] * pow(5, i);
-    return decimal;
-  }
-
-  string lookup = "=-012";
-  string decimalToSnafu(INT decimal) {
-    auto [q, r] = std::div(decimal + 2, (INT)5);
-    return (q ? decimalToSnafu(q) : "") + lookup[r];
-  }
-
   virtual void part1(ostream& out) override {
-    INT sum = 0;
-    for (string snafu : snafus) sum += snafuToDecimal(snafu);
-    out << decimalToSnafu(sum);
+    Snafu sum = 0;
+    for (string snafu : snafus) sum += snafu;
+    out << sum;
   }
 
   virtual void part2(ostream& out) override {}
